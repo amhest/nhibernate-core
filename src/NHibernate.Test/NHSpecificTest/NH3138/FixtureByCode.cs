54fb94746c338135b6d2204f244e41c2e5e58111
Gerke Geurts <ggeurts@geurtsrus.com>
2012-01-23 15:41:22 +0100

    NH-2977
    - Added limit SQL generation for SQL Server queries with comments, common table expressions, and stored procedure invocations.
    - Modified loader to fall back to client-side limit/offset behavior if dialect does not support generation of server side limit/offset SQL for a given (custom) SQL statement.
    
    Conflicts:
    	src/NHibernate/Dialect/MsSql2005DialectQueryPager.cs

--- src/NHibernate.Test/NHSpecificTest/NH3138/FixtureByCode.cs
+++ src/NHibernate.Test/NHSpecificTest/NH3138/FixtureByCode.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
+using NHibernate.Criterion;
 using NHibernate.Mapping.ByCode;
+using NHibernate.Transform;
 using NUnit.Framework;
 
 namespace NHibernate.Test.NHSpecificTest.NH3138
@@ -36,30 +39,189 @@ namespace NHibernate.Test.NHSpecificTest.NH3138
 						.CreateQuery("select distinct e.Id, coalesce(e.EnglishName, e.GermanName) from Entity e order by coalesce(e.EnglishName, e.GermanName) asc")
 						.SetFirstResult(10)
 						.SetMaxResults(20)
-						.List<Entity>());
+						.List());
 			}
 		}
 
-		[Test]
-		[Ignore("Failing")]
-		public void PageQueryWithDistinctAndOrderByContainingAliasedFunction()
-		{
-			using (var session = OpenSession())
-			{
-				Assert.DoesNotThrow(() =>
-					session
-						.CreateQuery("select distinct e.Id, coalesce(e.EnglishName, e.GermanName) as LocalizedName from Entity e order by LocalizedName asc")
-						.SetFirstResult(10)
-						.SetMaxResults(20)
-						.List<Entity>());
-			}
-		}
+      [Test]
+      [Ignore("Failing")] //This is failing because HQL -> SQL is failing to translate the alias in Order By
+	   public void PageQueryWithDistinctAndOrderByContainingAliasedFunction()
+	   {
+	      using (var session = OpenSession())
+	      {
+	         Assert.DoesNotThrow(() =>
+	                             session
+	                                .CreateQuery(
+	                                   "select distinct e.Id, coalesce(e.EnglishName, e.GermanName) as LocalizedName from Entity e order by LocalizedName asc")
+	                                .SetFirstResult(10)
+	                                .SetMaxResults(20)
+	                                .List<Entity>());
+	      }
+	   }
+
+	   [Test]
+	   public void PageQueryOverWithDistinctAndOrderByContainingAliasedFunction()
+	   {
+	      using (ISession session = OpenSession())
+	      {
+	         String LocalizedName = "LocalizedName";
+	         //Build a set of columns with a coalese
+	         ProjectionList plColumns = Projections.ProjectionList();
+	         plColumns.Add(Projections.Property<Entity>(x => x.Id), "Id");
+	         plColumns.Add(Projections.SqlFunction("coalesce",
+	                                               NHibernateUtil.String,
+	                                               Projections.Property<Entity>(x => x.EnglishName),
+	                                               Projections.Property<Entity>(x => x.GermanName))
+	                                  .WithAlias(() => LocalizedName));
+
+	         ProjectionList plDistinct = Projections.ProjectionList();
+	         plDistinct.Add(Projections.Distinct(plColumns));
+
+
+	         //Make sure we parse and run without error
+	         Assert.DoesNotThrow(() => session.QueryOver<Entity>()
+	                                          .Select(plDistinct)
+	                                          .TransformUsing(Transformers.AliasToBean<LocalizedEntity>())
+	                                          .OrderByAlias(() => LocalizedName).Asc
+	                                          .Skip(10)
+	                                          .Take(20)
+	                                          .List<LocalizedEntity>());
+
+
+	         //Now make sure we actually page and limit
+	         IList<LocalizedEntity> p1 = session.QueryOver<Entity>()
+	                                            .Select(plDistinct)
+	                                            .TransformUsing(Transformers.AliasToBean<LocalizedEntity>())
+	                                            .OrderByAlias(() => LocalizedName).Asc
+	            //
+	            // First Page
+	            //.Skip(10)
+	                                            .Take(20)
+	                                            .List<LocalizedEntity>();
+
+	         Assert.IsNotNull(p1);
+
+	         IList<LocalizedEntity> p2 = session.QueryOver<Entity>()
+	                                            .Select(plDistinct)
+	                                            .TransformUsing(Transformers.AliasToBean<LocalizedEntity>())
+	                                            .OrderByAlias(() => LocalizedName).Asc
+	            // Second Page
+	                                            .Skip(10)
+	                                            .Take(20)
+	                                            .List<LocalizedEntity>();
+
+	         Assert.IsNotNull(p2);
+	         Assert.IsTrue(p1.Count == 20);
+	         Assert.IsTrue(p2.Count == 20);
+
+	         Assert.NotNull(p1[0]);
+	         Assert.NotNull(p2[0]);
+
+	         Assert.AreNotEqual(p1[0].Id, p2[0].Id);
+	         Assert.AreNotEqual(p1[0].LocalizedName, p2[0].LocalizedName);
+	      }
+	   }
+
+	   [SetUp]
+	   public void SetUp()
+	   {
+	      using (ISession session = OpenSession())
+	      {
+	         using (ITransaction t = session.BeginTransaction())
+	         {
+	            for (int i = 0; i < (data.Length/2); i++)
+	            {
+	               var e = new Entity
+	                  {
+	                     Id = Guid.NewGuid(),
+	                     EnglishName = data[i, 0],
+	                     GermanName = data[i, 1]
+	                  };
+	               session.Save(e);
+	            }
+	            t.Commit();
+	         }
+	      }
+	   }
+
+	   [TearDown]
+	   public void TearDown()
+	   {
+	      using (ISession session = OpenSession())
+	      {
+	         using (ITransaction t = session.BeginTransaction())
+	         {
+	            //Truncate
+	            session.Delete("from Entity");
+	            t.Commit();
+	         }
+	      }
+	   }
+
+	   private readonly string[,] data =
+	      {
+	         {"Faber", null},
+	         {null, "dyer"},
+	         {"Fassbinder", "cooper"},
+	         {"Faust", "fist"},
+	         {"Feierabend", null},
+	         {"Fenstermacher", "window maker"},
+	         {"Fiedler", "fiddler"},
+	         {null, "finch"},
+	         {"Fleischer", "butcher"},
+	         {"Foerster", "forester"},
+	         {"Frankfurter", "of Frankfurt"},
+	         {null, "Friday"},
+	         {"Freud", "joy"},
+	         {"Frei", null},
+	         {"Freeh", null},
+	         {"Fruehauf", "up early"},
+	         {"Fuchs", "fox"},
+	         {"Fuerst/Furst", "prince"},
+	         {"Fuhrmann", "carter, driver"},
+	         {null, "gardener"},
+	         {"Gerber", "tanner"},
+	         {"Gerste/Gersten", "barley"},
+	         {"Gloeckner/Glockner", "bell man"},
+	         {"Gottschalk", "God's servant"},
+	         {null, "green forest"},
+	         {"Hertz/Herz", "heart"},
+	         {"Hertzog/Herzog", "duke"},
+	         {null, "heaven"},
+	         {"Hirsch", "buck, deer"},
+	         {"Hoch", null},
+	         {"Jaeger", null},
+	         {"Jung", "young"},
+	         {"Junker", "nobleman, squire"},
+	         {"Kaiser", "emperor"},
+	         {"Kalb", "calf"},
+	         {null, "cabinet maker"},
+	         {"Kappel", "chapel"},
+	         {"Kaufmann", "merchant"},
+	         {"Kirsch", "cherry"},
+	         {"Klein", "short, small"},
+	         {"Klug/Kluge", null},
+	         {"Koch", "cook"},
+	         {null, "charcoal-maker"},
+	         {"Koenig/Konig", "king"},
+	         {"Krause", "curly haired"},
+	         {"Kuefer", "cooper"},
+	         {null, "sexton"},
+	         {"Kuhn/Kunze", null}
+	      };
 	}
 
-	class Entity
-	{
-		public Guid Id { get; set; }
-		public string EnglishName { get; set; }
-		public string GermanName { get; set; }
-	}
+   internal class Entity
+   {
+      public Guid Id { get; set; }
+      public string EnglishName { get; set; }
+      public string GermanName { get; set; }
+   }
+
+
+   internal class LocalizedEntity
+   {
+      public Guid Id { get; set; }
+      public string LocalizedName { get; set; }
+   }
 }
